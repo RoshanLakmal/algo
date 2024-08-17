be2402ec9f641ee74831379719476deaa92a1b5e
Roshan <roshanrox911@gmail.com>
2024-08-17 11:18:34 +1000

    reversting

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reverse = ""
+    for(i=str.length-1;i>=0;i--){
+        reverse += str[i]
+    }
+    return reverse
+}
 
 module.exports = reverse;
